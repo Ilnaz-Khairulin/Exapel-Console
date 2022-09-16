8778319919410637af133a56d00de51c145ff19b
Ilnaz <72466445@mail.ru>
2022-09-16 16:17:11 +0500

    If

---
+++ Exampel001_HelloConsole/Exampel005_ifels/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura eto Masha");
+}
+else
+{
+    Console.Write("Privet ");
+    Console.WriteLine(username);
+}
